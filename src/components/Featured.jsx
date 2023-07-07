eaa8646af8a8b987051742fd7a359f3b680189cb
MangGlang <71730799+MangGlang@users.noreply.github.com>
2023-07-07 08:26:37 -0700

    Featured Books

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import Book from './ui/Book';
+import React from "react";
+import Book from "./ui/Book";
+import { books } from "../data";
 
 const Featured = () => {
-    return (
-        <div>
-            <section id="features">
-                <div className="container">
-                    <div className="row">
-                        <h2 className="section__title">
-                            Featured <span className="purple">Books</span>
-                        </h2>
-                        <div className="books">
-                            <Book />
-                            <Book />
-                            <Book />
-                            <Book />
-                        </div>
-                    </div>
-                </div>
-            </section>
+  console.log(books);
+  return (
+    <div>
+      <section id="features">
+        <div className="container">
+          <div className="row">
+            <h2 className="section__title">
+              Featured <span className="purple">Books</span>
+            </h2>
+            <div className="books">
+              {books
+                .filter((book) => book.rating === 5)
+                .slice(0, 4)
+                .map((book) => (
+                    // Passing book as propName & accepting param (propValue) as book
+                  <Book book={book} key={book.id} />
+                ))}
+            </div>
+          </div>
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
 export default Featured;
--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,35 +1,41 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
-const Book = () => {
+const Book = ({ book }) => {
+    const rating = 4;
+
   return (
-    <div>
-      <div className="book">
-        <a href="">
-          <figure className="book__img--wrapper">
-            <img
-              src="https://covers.openlibrary.org/b/id/8091016-L.jpg"
-              alt=""
-              className="book"
-            />
-          </figure>
+    <div className="book">
+      <a href="">
+        <figure className="book__img--wrapper">
+          <img src={book.url} alt="" className="book" />
+        </figure>
+      </a>
+      <div className="book__title">
+        <a href="/" className="book__title--link">
+          {book.title}
         </a>
-        <div className="book__title">
-          <a href="/" className="book__title--link">
-            Atomic Habits
-          </a>
-        </div>
-        <div className="book__ratings">
-          <FontAwesomeIcon icon="star" />
-          <FontAwesomeIcon icon="star" />
-          <FontAwesomeIcon icon="star" />
-          <FontAwesomeIcon icon="star" />
-          <FontAwesomeIcon icon="star-half-alt" />
-        </div>
-        <div className="book__price">
-          <span className="book__price--normal">$15.00</span>
-          $10.00
-        </div>
+      </div>
+      <div className="book__ratings">
+        {
+            // underscore "_" element = "useless" element
+            new Array(5).fill(0).map((_, index) => <FontAwesomeIcon icon="star" key={index}/>)
+        }
+      </div>
+      <div className="book__price">
+        {/* Does book sale price exist? */}
+        {/* If sale exists, print original and sale price */}
+        {/* If no sale, then print only original price. */}
+        {book.salePrice ? (
+          <>
+            <span className="book__price--normal">
+              ${book.originalPrice.toFixed(2)}
+            </span>
+            ${book.salePrice.toFixed(2)}
+          </>
+        ) : (
+          <>${book.originalPrice}</>
+        )}
       </div>
     </div>
   );
