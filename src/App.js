2b660386c26947d31a4058a6d822e42cc68140ed
MangGlang <71730799+MangGlang@users.noreply.github.com>
2023-07-09 16:55:54 -0700

    Non-dynamic Cart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Books from "./pages/Books.jsx";
 import { books } from "./data";
 import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
 
 
 function App() {
@@ -13,9 +14,12 @@ function App() {
     <Router>
       <div className="App">
         <Nav />
+        
         <Route path="/" exact component={Home} />
         <Route path="/books" exact render={() => <Books books={books}/>} />
         <Route path="/books/:id" render={() => <BookInfo books={books} />} />
+        {/* Passing in props requires the use of "render" & arrow functions */}
+        <Route path="/cart" render={() => <Cart books={books}/>} />
         <Footer />
       </div>
     </Router>
